04fe521081b8aeea0549c8c964ef936d4f476c69
muratozbk <slipstream11@yandex.com>
2023-05-17 18:33:20 +0300

    display users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
     const [users, setUsers] = useState([])
@@ -11,18 +12,22 @@ function UserResults() {
 
     const fetchUsers = async () => {
         const res =
-            await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`)
+            await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+                headers: {
+                    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+                },
+            })
         const data = await res.json()
 
         setUsers(data)
         setLoading(false)
-
     }
     if (!loading) {
         return <div className="grid grid-cols-1 gap-8
      xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-            {users.map((user, id) => (
-                <h3 key={id}>{user.login}</h3>
+            {users.map((user) => (
+                <UserItem key={user.id} user={user} />
+                // <h3 key={user.id}>{user.login}</h3>
             )
             )}
         </div>
