1a96a4941e45e5af4421e69eda39d348e2309333
muratozbk <slipstream11@yandex.com>
2023-05-20 19:56:16 +0300

    refactered

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,10 @@
+import axios from "axios"
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 // const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+const github = axios.create({
+    baseURL: GITHUB_URL
+})
 
 //Get search results
 export const searchUsers = async (text) => {
@@ -7,36 +12,43 @@ export const searchUsers = async (text) => {
         q: text
     })
 
-    const res = await
-        fetch(`${GITHUB_URL}/search/users?${params}`)
-
-    const { items } = await res.json()
-
-    return items
+    const res = await github.get(`/search/users?${params}`)
+    return res.data.items
 }
 
-//Get a Single User
-export const getUser = async (login) => {
-    const res = await fetch(`${GITHUB_URL}/users/${login}`)
+// Get user and repos
+export const getUserAndRepos = async (login) => {
+    const [user, repos] = await Promise.all([
+        github.get(`/users/${login}`),
+        github.get(`/users/${login}/repos`)
+    ])
 
-    if (res.status === 404) {
-        window.location = '/notfound'
-    } else {
-        const data = await res.json()
-        return data
-    }
+    return { user: user.data, repos: repos.data }
 }
 
-// Get User Repos
-export const getUserRepos = async (login) => {
-    const params = new URLSearchParams({
-        sort: 'created',
-        per_page: 10
-    })
 
-    const res = await
-        fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)
+// //Get a Single User
+// export const getUser = async (login) => {
+//     const res = await fetch(`${GITHUB_URL}/users/${login}`)
 
-    const data = await res.json()
-    return data
-}
+//     if (res.status === 404) {
+//         window.location = '/notfound'
+//     } else {
+//         const data = await res.json()
+//         return data
+//     }
+// }
+
+// // Get User Repos
+// export const getUserRepos = async (login) => {
+//     const params = new URLSearchParams({
+//         sort: 'created',
+//         per_page: 10
+//     })
+
+//     const res = await
+//         fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)
+
+//     const data = await res.json()
+//     return data
+// }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,20 +6,20 @@ function About() {
                 A React app to search Github profiles and see profile
                 details. This
                 project is part of the
-                <a className="text-accent" href="https://www.udemy.com/course/modern-react-front-to-back/">
+                <a className="text-accent" href="https://www.udemy.com/course/modern-react-front-to-back/" rel="noreferrer">
                     {' '}
                     React Front To Back
                 </a>{' '}
                 Udemy course by
                 <strong>
-                    <a href="https://traversymedia.com"> Brad Traversy</a>
+                    <a href="https://traversymedia.com" rel="noreferrer"> Brad Traversy</a>
                 </strong>.</p>
             <p className="text-lg text-gray-400">Version
                 <span className="text-white"> 1.0.0</span>
             </p>
             <p className="text-lg text-gray-400">
                 Layout By:
-                <a className="text-white" href="https://daisyui.com/" target="_blank">
+                <a className="text-white" href="https://daisyui.com/" target="_blank" rel="noreferrer">
                     {' '}  <span className="text-primary">daisy</span>UI
                 </a>
             </p>
